cc835d9b0d758573db2f3ca121239bf1b02f93ad
MariiaAmbr <soul8888@gmail.com>
2022-11-22 23:48:51 +0200

    Added axios and react-loader-spinner

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Weather from "./Weather";
+
 import "./App.css";
 
 function App() {
@@ -7,7 +8,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Hello World</h1>
-        <Weather />
+        <Weather city="Tokyo" />
       </header>
     </div>
   );
--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,18 @@
 import React from "react";
+import axios from "axios";
+import { Audio } from "react-loader-spinner";
 
-export default function Weather() {
-  return <h2>Hello from Weather!</h2>;
+export default function Weather(props) {
+  function handleResponse(response) {
+    alert(
+      `The weather in ${response.data.name} is ${response.data.main.temp}°C`
+    );
+  }
+  let apiKey = "3ef05a16171b60ea065a0ce29b7252a6";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=
+   ${props.city}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(handleResponse);
+
+  return <Audio heigth="100" width="100" color="grey" ariaLabel="loading" />;
 }
